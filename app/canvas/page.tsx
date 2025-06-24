66fe8a1b4bf34f77b0c4eb79a15c3b292215b19d
SubhenduBarik-RWS <97940985+SubhenduBarik-RWS@users.noreply.github.com>
2025-06-25 01:35:57 +0530

    removed some logs

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -1,13 +1,8 @@
 'use client'
 import {ApolloProvider} from "@apollo/client";
 import client from "@/db/apolloClient";
-import {loadDevMessages, loadErrorMessages} from "@apollo/client/dev";
 import Workspace from "@/components/Workspace";
 import {Toaster} from 'sonner';
-
-loadDevMessages();
-loadErrorMessages();
-
 
 export default function Canvas() {
     return (
